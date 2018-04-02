cf34629dcb6a4738685f78346fa661e05e770488
nandoseptianhusni <nandohusni93@gmail.com>
2018-04-03 04:24:40 +0700

    Initial Commit

---
+++ Slider Image Scrollview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slider Image Scrollview
+//
+//  Created by Nando Septian Husni on 4/3/18.
+//  Copyright © 2018 imastudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
